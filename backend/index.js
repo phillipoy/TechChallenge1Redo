781feaa06aa4c89258ea38b6a2226489270b551d
Phillip Oyediran <phillipoyediran@gmail.com>
2026-05-17 04:03:07 -0400

    Update frontend API path for ECS ALB routing

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,29 @@
 const express = require('express')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 const { CORS_ORIGIN } = require('./config')
-console.log(require('./config'))
-console.log(CORS_ORIGIN)
 
 const ID = uuidv4()
 const PORT = 5000
 
 const app = express()
+
 app.use(express.json())
 
+// Allows frontend requests through the ALB
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN)
     res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.setHeader('Access-Control-Allow-Headers', '*')
-    next();
-})
-app.get(/.*/, (req, res) => {
-    console.log(`${new Date().toISOString()} GET`)
-    res.json({id: ID})
+    next()
 })
 
+// Backend API route used by the frontend
+app.get('/api/', (req, res) => {
+    console.log(`${new Date().toISOString()} GET /api/`)
+    res.json({ id: ID })
+})
+
+// Starts the Express backend server
 app.listen(PORT, () => {
     console.log(`Backend started on ${PORT}. ctrl+c to exit`)
 })
